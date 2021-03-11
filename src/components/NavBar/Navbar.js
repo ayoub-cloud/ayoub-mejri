20abb16b7f0afaac4a2476f70e3771729760e4d4
ayoub-cloud <ayoubb06@hotmail.com>
2021-03-11 17:30:38 +0100

    first-commit

---
+++ src/components/NavBar/Navbar.js
@@ -0,0 +1,22 @@
+import "./Navbar.css"
+
+function Navbar() {
+return (
+    <nav>
+          <ul className="Firstapp" >
+              <li>Home</li>
+              <li>Contact</li>
+              <li>About us</li>
+
+          </ul>
+    </nav>
+
+
+
+
+
+
+
+)
+}
+export default Navbar
